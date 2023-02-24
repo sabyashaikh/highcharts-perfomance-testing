b7efd256fe03c7764ba297aeaec259244d7142b6
Sabya Shaikh <sabya.shaikh@baywa-re.com>
2023-02-24 14:46:46 +0100

    Removed unecessary imports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,6 @@
 import HighchartHeatMap from "../components/HighchartHeatMap/HighchartHeatMap";
 
 function Home() {
-    const heatmap_dp_1k = require("../components/HighchartHeatMap/heat_map_data_points_1k.json")
-    const heatmap_dp_50k = require("../components/HighchartHeatMap/heat_map_data_points_50k.json")
-    const heatmap_dp_100k = require("../components/HighchartHeatMap/heat_map_data_points_100k.json")
-    const heatmap_dp_300k = require("../components/HighchartHeatMap/heat_map_data_points_300k.json")
     return (
         <div>
             Choose chart type
